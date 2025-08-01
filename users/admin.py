cf98cb02b4dab815437f21a720aa6952e1346968
Dmitry Minin <d.n.minin@gmail.com>
2025-08-01 19:53:02 +0200

    - добавил блок с уcтановкой приложения и основные команды - зарегистрировал модели в админке - немного поменял форму регистрации теперь отображается поле аватара -в базовом шаблоне добавил отображение аватара

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "avatar", "is_staff", "is_active", "is_superuser")
+    list_filter = ("is_staff", "is_active", "is_superuser")
+    search_fields = ("username", "email")
+    ordering = ("username",)
